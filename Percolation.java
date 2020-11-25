5abe80a78a35876405d6f90d5b0d4507b8785883
Shane Richards <shane.richards@fticonsulting.com>
2020-11-25 16:37:03 -0500

    completed the percolation constructor

--- Percolation.java
+++ Percolation.java
@@ -4,9 +4,32 @@
  *  Last modified:     1/1/2019
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
+    private int rowWidth;
+    private WeightedQuickUnionUF quickUnionUF;
+
     // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n) { }
+    public Percolation(int n) {
+        if (n <= 0) throw new IllegalArgumentException("n cannot be less than 0");
+
+        rowWidth = n;
+        int cellCount = n * n + 2;
+
+        quickUnionUF = new WeightedQuickUnionUF(cellCount);
+
+        int virtualTopNode = 0;
+        int virtualLastNode = cellCount - 1;
+
+        for (int i = 0; i < rowWidth; i++) {
+            int topRowCell = mapCoordinateToIndex(0, i);
+            int bottomRowCell = mapCoordinateToIndex(rowWidth - 1, i);
+
+            quickUnionUF.union(virtualTopNode, topRowCell);
+            quickUnionUF.union(virtualLastNode, bottomRowCell);
+        }
+    }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) { }
@@ -24,5 +47,16 @@ public class Percolation {
     public boolean percolates() { return false; }
 
     // test client (optional)
-    public static void main(String[] args) { }
+    public static void main(String[] args) {
+        Percolation percolation = new Percolation(3);
+    }
+
+    /*
+    (0, 0) will map to the  virtual top at index 0
+     */
+    private int mapCoordinateToIndex(int row, int col) {
+        if (row > rowWidth - 1 || col > rowWidth - 1) throw new IllegalArgumentException();
+
+        return (row * rowWidth) + col + 1;
+    }
 }
