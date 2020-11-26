80083cfe1781bf9b147e92caf8cd5302f093d61f
Shane Richards <shane.richards@fticonsulting.com>
2020-11-26 14:17:27 -0500

    compeleted the percolation stats class

--- PercolationStats.java
+++ PercolationStats.java
@@ -4,22 +4,81 @@
  *  Last modified:     1/1/2019
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
+    private double[] results;
+
     // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials) { }
+    public PercolationStats(int n, int trials) {
+        if (n < 1 || trials < 1) {
+            throw new IllegalArgumentException("n and trials must be greater than 0");
+        }
+
+        results = new double[trials];
+        int totalNumberOfSites = n * n;
+
+        for (int i = 0; i < trials; i++) {
+            Percolation percolation = new Percolation(n);
+
+            while (!percolation.percolates()) {
+                int row = StdRandom.uniform(1, n + 1);
+                int col = StdRandom.uniform(1, n + 1);
+
+                percolation.open(row, col);
+            }
+
+            results[i] = (double) percolation.numberOfOpenSites() / totalNumberOfSites;
+        }
+    }
 
     // sample mean of percolation threshold
-    public double mean() { return 0; }
+    public double mean() {
+        return StdStats.mean(results);
+    }
 
     // sample standard deviation of percolation threshold
-    public double stddev() { return 0; }
+    public double stddev() {
+        return StdStats.stddev(results);
+    }
 
     // low endpoint of 95% confidence interval
-    public double confidenceLo() { return 0; }
+    public double confidenceLo() {
+        return mean() - partialConfidence();
+    }
 
     // high endpoint of 95% confidence interval
-    public double confidenceHi() { return 0; }
+    public double confidenceHi() {
+        return mean() + partialConfidence();
+    }
+
+    private double partialConfidence() {
+        return ((1.96 * stddev()) / Math.sqrt(results.length));
+    }
 
     // test client (see below)
-    public static void main(String[] args) { }
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);         // n-by-n percolation system
+        int trails = Integer.parseInt(args[1]);    // number of trails
+
+        PercolationStats stats = new PercolationStats(n, trails);
+
+        double mean = stats.mean();
+        double stddev = stats.stddev();
+        double confidenceLo = stats.confidenceLo();
+        double confidenceHi = stats.confidenceHi();
+
+        // String output = String.format("mean\t\t= %f\nstddev\t\t= %f\n95%% confidence interval\t= (%f, %f)",
+        //                               mean,
+        //                               stddev,
+        //                               confidenceLo,
+        //                               confidenceHi);
+
+        String output = "mean\t\t\t\t\t= " + mean + "\n"
+                + "stddev\t\t\t\t\t= " + stddev + "\n"
+                + "95% confidence interval\t= [" + confidenceLo + ", " + confidenceHi + "]";
+        System.out.println(
+                output);
+    }
 }
