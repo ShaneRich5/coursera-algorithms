b4fc0778973fa0c059344cde943f64a10282a492
Shane <shane.richards121@gmail.com>
2020-12-09 03:44:57 -0500

    completed the week 2 required assignment

---
+++ Permutation.java
@@ -0,0 +1,17 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Permutation {
+    public static void main(String[] args) {
+        RandomizedQueue<String> queue = new RandomizedQueue<String>();
+        int take = Integer.parseInt(args[0]);
+
+        while (!StdIn.isEmpty()) {
+            queue.enqueue(StdIn.readString());
+        }
+
+        for (int i = 0; i < take; i++) {
+            StdOut.println(queue.dequeue());
+        }
+    }
+}
